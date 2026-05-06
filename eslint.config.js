8812a6bc6ae33540a6cd98eb9474b4930b8ebcec
sefriadiahmad <sefriadiahmad@gmail.com>
2026-05-06 19:05:18 +0700

    fix bug 2

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
 import globals from "globals";
 import pluginJs from '@eslint/js';
 import daStyle from 'eslint-config-dicodingacademy';
- 
- 
+
+
 export default [
   daStyle,
   { files: ['**/*.js'], languageOptions: { sourceType: 'module' } },
