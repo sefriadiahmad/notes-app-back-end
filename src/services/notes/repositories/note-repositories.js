5aace833ed1c0784f36d43ce71c0b76f13818a58
sefriadiahmad <sefriadiahmad@gmail.com>
2026-05-16 10:14:57 +0700

    authorization implemented

--- src/services/notes/repositories/note-repositories.js
+++ src/services/notes/repositories/note-repositories.js
@@ -7,14 +7,14 @@ class NoteRepositories {
   }
 
 
-  async createNote({ title, body, tags }) {
+  async createNote({ title, body, tags, owner }) {
     const id = nanoid(16);
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
 
     const query = {
-      text: 'INSERT INTO notes(id, title, body, tags, created_at, updated_at) VALUES($1, $2, $3, $4, $5, $6) RETURNING id, title, body, tags, created_at, updated_at',
-      values: [id, title, body, tags, createdAt, updatedAt],
+      text: 'INSERT INTO notes(id, title, body, tags, owner, created_at, updated_at) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id, title, body, tags, owner, created_at, updated_at',
+      values: [id, title, body, tags, owner, createdAt, updatedAt],
     };
 
     const result = await this.pool.query(query);
@@ -22,9 +22,10 @@ class NoteRepositories {
     return result.rows[0];
   }
 
-  async getNotes() {
+  async getNotes(owner) {
     const query = {
-      text: 'SELECT * FROM notes',
+      text: 'SELECT * FROM notes WHERE owner = $1',
+      values: [owner],
     };
     const result = await this.pool.query(query);
     return result.rows;
@@ -63,6 +64,25 @@ class NoteRepositories {
     const result = await this.pool.query(query);
 
     return result.rows[0].id;
+  }
+
+  async verifyNoteOwner(id, owner) {
+    const query = {
+      text: 'SELECT * FROM notes WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this.pool.query(query);
+    if (!result.rows.length) {
+      return null;
+    }
+
+    const note = result.rows[0];
+    if (note.owner !== owner) {
+      return null;
+    }
+
+    return result.rows[0];
   }
 }
 
